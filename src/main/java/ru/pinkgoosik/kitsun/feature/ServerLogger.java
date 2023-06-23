5d15fb93a6624a40e0e1259271aa7998192bda61
PinkGoosik <MCGycuk@yandex.ru>
2023-06-23 15:31:04 +0500

    update jda

--- src/main/java/ru/pinkgoosik/kitsun/feature/ServerLogger.java
+++ src/main/java/ru/pinkgoosik/kitsun/feature/ServerLogger.java
@@ -42,7 +42,7 @@ public class ServerLogger {
 
 	public void onMemberJoin(Member member) {
 		var embed = new EmbedBuilder();
-		embed.setAuthor(member.getUser().getAsTag().replace("#0000", "") + " joined", null, member.getUser().getEffectiveAvatarUrl());
+		embed.setAuthor(member.getUser().getName() + " joined", null, member.getUser().getEffectiveAvatarUrl());
 		embed.setColor(KitsunColors.getGreen());
 		embed.setTimestamp(Instant.now());
 		log(embed.build());
@@ -50,7 +50,7 @@ public class ServerLogger {
 
 	public void onMemberLeave(User user) {
 		var embed = new EmbedBuilder();
-		embed.setAuthor(user.getAsTag().replace("#0000", "") + " left", null, user.getEffectiveAvatarUrl());
+		embed.setAuthor(user.getName() + " left", null, user.getEffectiveAvatarUrl());
 		embed.setColor(KitsunColors.getRed());
 		embed.setTimestamp(Instant.now());
 		log(embed.build());
